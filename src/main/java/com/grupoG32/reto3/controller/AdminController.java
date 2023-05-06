285baaac55ebc284114992e54b775abb0ee28f0f
Sebastian Cepeda <scepeda192@gamil.com>
2023-05-05 21:03:29 -0500

    Se crean los modelos

---
+++ src/main/java/com/grupoG32/reto3/controller/AdminController.java
@@ -0,0 +1,27 @@
+package com.grupoG32.reto3.controller;
+
+
+import com.grupoG32.reto3.model.AdminModel;
+import com.grupoG32.reto3.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/Admin")
+public class AdminController {
+
+    @Autowired
+    AdminService adminService;
+
+    @GetMapping("/all")
+    public List<AdminModel> obtenerAdministradores(){
+        return adminService.obtenerAdministradores();
+    }
+
+    @PostMapping("/save")
+    public String crearAdministradores(@RequestBody AdminModel adminModel){
+        return adminService.crearAdministradores(adminModel);
+    }
+}
